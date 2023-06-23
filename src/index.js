a37883c8805ad9719d4225313ac72240a0cac962
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-06-23 17:52:39 +0300

    app_ne_potriben

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import user from "./data/user.json";
 import data from "./data/data.json";
 import friends from "./data/friends.json";
 import transactions from "./data/transactions.json";
-import { App } from 'components/App/App';
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics'
 import { FriendList } from 'components/Friendlist/Friendlist';
