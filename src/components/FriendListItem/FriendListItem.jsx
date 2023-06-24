6a39a84d21a084c7fec2b34a7caeb433d3e23b92
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-06-24 17:56:14 +0300

    prop-types for friendlistitem

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,5 +1,13 @@
+import propTypes from 'prop-types'
 import {FriendListLi,SpanStatusOnline,SpanStatusOffline,FriendAvatar,FriendName} from 'components/Friendlist/Friendlist.styled'
 
 export const FriendListItem = ({avatar,name,isOnline}) => 
 <FriendListLi>{isOnline === true?<SpanStatusOnline></SpanStatusOnline>:<SpanStatusOffline></SpanStatusOffline>}
         <FriendAvatar src={avatar} alt="User avatar" width="48"></FriendAvatar><FriendName>{name}</FriendName></FriendListLi>
+
+
+FriendListItem.propTypes = {
+        avatar: propTypes.string.isRequired,
+        name: propTypes.string.isRequired,
+        isOnline: propTypes.bool.isRequired,
+}
